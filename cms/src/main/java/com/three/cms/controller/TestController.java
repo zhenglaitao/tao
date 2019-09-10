c176bf46821cbf6a4c2fa948658deb444fd15c88
zlt <604757602@qq.com>
2019-09-10 10:54:14 +0800

    first beetl test  commit

--- cms/src/main/java/com/three/cms/controller/TestController.java
+++ cms/src/main/java/com/three/cms/controller/TestController.java
@@ -3,6 +3,8 @@ package com.three.cms.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author zlt
  */
@@ -12,5 +14,11 @@ public class TestController {
     @GetMapping("/")
     public String testIndex(){
         return "index";
+    }
+
+    @GetMapping("/testBeetl")
+    public String testBeetl(HttpServletRequest request){
+        request.setAttribute("aaa","diyici");
+        return "test.html";
     }
 }
